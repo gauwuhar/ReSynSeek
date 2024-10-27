f29fc85e62b6a68a2a1efc09618317549d584a07
5ArLaN5 <arlan5yesengaziyev@gmail.com>
2024-10-27 18:34:33 +0500

    Correctly added Frontend repository.

--- ReSynSeek/Frontend/src/app/logger.interceptor.ts
+++
@@ -1,6 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const loggerInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log(`hello ${req.url}`);
-  return next(req);
-};
